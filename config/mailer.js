8f3639bb4a5ac7e242d72f561bb5d724d2eddf6a
Usman Sarwar <sarwarusman708@gmail.com>
2025-12-26 17:27:21 +0500

    edited zohomail configuration to use port 465 for SSL connection 	modified:   config/mailer.js 	deleted:    utils/sendEmail.js

--- config/mailer.js
+++ config/mailer.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.zoho.com",
-  port: 587,
-  secure: false, // TLS via STARTTLS
+  port: 465,
+  secure: true, // TLS via STARTTLS
   auth: {
     user: process.env.MAIL_USER, // support@vircosa.com
     pass: process.env.MAIL_PASS  // Zoho App Password
